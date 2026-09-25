ddd02cfb78ae89326a1cb07562781d9dd108c7d8
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-25 23:39:38 +0800

    Count Non-Null Values with COUNT(column)

--- Aggregate-Functions-GROUP-BY-and-HAVING.sql
+++ Aggregate-Functions-GROUP-BY-and-HAVING.sql
@@ -6,3 +6,13 @@ FROM customers;
 -- Result:
 -- total_customers
 -- 5
+
+
+-- Example 2 — Count Non-Null Values with COUNT(column)
+-- GOAL: Count how many customers have provided an email address.
+SELECT COUNT(email) AS customers_with_email
+FROM customers;
+
+-- Result:
+-- customers_with_email
+-- 4
