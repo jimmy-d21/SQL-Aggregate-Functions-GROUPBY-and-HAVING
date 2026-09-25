4641839d2ad8d493bb107e1ef953c8f580d940fb
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-25 23:38:56 +0800

    Count All Rows with COUNT(*)

---
+++ Aggregate-Functions-GROUP-BY-and-HAVING.sql
@@ -0,0 +1,8 @@
+-- Example 1 — Count All Rows with COUNT(*)
+-- GOAL: Example 1 — Count All Rows with COUNT(*)
+SELECT COUNT(*) AS total_customers
+FROM customers;
+
+-- Result:
+-- total_customers
+-- 5
